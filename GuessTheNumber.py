cdc470e641a0af0b25c6822d79c8ec409bd2992b
azmathias <azramathias@stuperuser.onmicrosoft.com>
2019-10-29 13:22:50 +0000

    Add GuessTheNumber

---
+++ GuessTheNumber.py
@@ -0,0 +1,25 @@
+# Guess the number game
+# Use has 5 guesses
+
+
+import random
+
+print('Hello. What is your name?')
+name = input()
+secNum = random.randint(1,20)
+print("\nWell, " + name + ", I'm thinking of a number between 1 and 20")
+
+# Player has 5 guesses
+for guessTaken in range(1,6):
+	print('Take a guess.')
+	guess = int(input())
+	if guess < secNum:
+		print('Your guess is too low.')
+	elif guess > secNum:
+		print('Your guess is too high')
+	else:
+		break
+if guess == secNum:
+	print('Well done ' + name + '! You guessed correctly')
+else:
+	print('No. The number I was thinking of was ' + str(secNum))
